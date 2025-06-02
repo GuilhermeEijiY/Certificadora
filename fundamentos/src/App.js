d73304473da04a3acf800814083b99b4ac718f1d
GuilhermeEijiY <guilherme-yoshida@hotmail.com>
2025-06-02 10:18:13 -0300

    Correção para sempre ir para a página home
    Foi trocado o browserRouter para o HashRouter para evitar a a necessidade de ser reescrita a URL e para melhor manipulação do site do github pages que tem arquivos estáticos

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -1,10 +1,8 @@
-import {BrowserRouter, Routes,Route, Navigate} from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Importe HashRouter e renomeie para Router para manter a consistência
 import './App.css';
-
 
 //pages
 import Home from './pages/Home';
-
 
 //components
 import Navbar from './components/Navbar';
@@ -18,18 +16,18 @@ import GuiaBoasPraticas from './pages/GuiaBoasPraticas';
 function App() {
   return (
     <div className="App">
-        <BrowserRouter>
-          <Navbar/>
+      <Router>
+        <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>}/>
-            <Route path='/Fundamentos' element={<Fundamentos/>}/>
-            <Route path='/Exercicios' element={<Exercicios/>}/>
-            <Route path='/Desafios' element={<Desafios/>}/>
-            <Route path='/SimuladorPassoAPasso' element={<SimuladorPassoAPasso/>}/>
-            <Route path='/GuiaBoasPraticas' element={<GuiaBoasPraticas/>}/>
+          <Route path='/Fundamentos' element={<Fundamentos/>}/>
+          <Route path='/Exercicios' element={<Exercicios/>}/>
+          <Route path='/Desafios' element={<Desafios/>}/>
+          <Route path='/SimuladorPassoAPasso' element={<SimuladorPassoAPasso/>}/>
+          <Route path='/GuiaBoasPraticas' element={<GuiaBoasPraticas/>}/>
         </Routes>
-          <Footer />
-      </BrowserRouter>
+        <Footer />
+      </Router>
     </div>
   );
 }
